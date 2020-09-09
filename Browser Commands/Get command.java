27892025ba8144b86e6ed62f46cd6ceaba4cc88f
Aditya Gujjar <70464438+AdityaGujjar@users.noreply.github.com>
2020-09-09 20:45:23 +0530

    Create Get command.java

---
+++ Browser Commands/Get command.java
@@ -0,0 +1,31 @@
+package browser_command;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Get_Command {
+
+	public static void main(String[] args) {
+		// Setting System properties
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		
+		// Initiating Chrome Driver
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		// Starting the program
+		
+		driver.navigate().to("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		
+		// Using "Get command" to start a new tab
+		
+		driver.get("https://www.youtube.com/?gl=IN");
+		
+		//Notes: Here the driver initiated Youtube in the same tab replacing Facebook
+		
+
+	}
+
+}
